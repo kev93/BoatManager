ca7c15372d0d0979897bd5a07886618deb940b39
kev93 <kevin.fuerst@gawnet.ch>
2025-04-15 15:20:45 +0200

    Add boat model and controller that returns some dummy data for now

---
+++ BoatManager/BoatManager.Backend/Controllers/BoatController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BoatManager.Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BoatController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Boat>>> GetBoats()
+    {
+        var boats = new List<Boat>();
+        boats.Add(new Boat(1, "my fancy boat", "this is a really nice boat"));
+        boats.Add(new Boat(2, "Boat 2", "also a really nice boat"));
+        boats.Add(new Boat(3, "Boat 3", "this is not so nice"));
+
+        return Ok(boats);
+    }
+}
---
+++ BoatManager/BoatManager.Backend/Models/Boat.cs
@@ -0,0 +1,15 @@
+﻿namespace BoatManager.Backend;
+
+public class Boat
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Details { get; private set; }
+
+    public Boat(int id, string name, string details)
+    {
+        Id = id;
+        Name = name;
+        Details = details;        
+    }
+}
