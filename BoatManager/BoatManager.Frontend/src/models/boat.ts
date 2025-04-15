cdbb0571e3864f0e964d652cd08a299436af4ee6
kev93 <kevin.fuerst@gawnet.ch>
2025-04-15 15:00:37 +0200

    Add boat model

---
+++ BoatManager/BoatManager.Frontend/src/models/boat.ts
@@ -0,0 +1,11 @@
+export class Boat {
+  id: number;
+  name: string;
+  details: string;
+
+  constructor(id: number, name: string, details: string) {
+    this.id = id;
+    this.name = name;
+    this.details = details;
+  }
+}
