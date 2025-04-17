090ed5b01c16b1886e99005c6cbecec26776868a
kev93 <kevin.fuerst@gawnet.ch>
2025-04-17 14:22:40 +0200

    Implement all CRUD operations on endpoint

--- BoatManager/BoatManager.Backend/Controllers/BoatController.cs
+++ BoatManager/BoatManager.Backend/Controllers/BoatController.cs
@@ -18,4 +18,33 @@ public class BoatController : ControllerBase
     {
         return Ok(await _context.Boats.ToListAsync());
     }
+
+    [HttpPost]
+    public async Task<ActionResult<Boat>> CreateBoat([FromBody] Boat boat)
+    {
+        _context.Boats.Add(boat);
+        await _context.SaveChangesAsync();
+        return Ok(boat);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<Boat>> UpdateBoat([FromBody] Boat boat)
+    {
+        if (await _context.Boats.AnyAsync(s => s.Id == boat.Id) == false)
+            return NotFound();
+        _context.Boats.Update(boat);
+        await _context.SaveChangesAsync();
+        return Ok(boat);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteBoat([FromRoute] Guid id)
+    {
+        var boat = await _context.Boats.FindAsync(id);
+        if(boat == null)
+            return NotFound();
+        _context.Boats.Remove(boat);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
 }
